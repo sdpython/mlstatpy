816564acf089a047adbd9561379141e986796fab
xavier dupré <xavier.dupre@gmail.com>
2017-02-07 01:59:53 +0100

    add unit test to test peregrination

---
+++ _unittests/ut_documentation/test_run_notebooks_dsgarden.py
@@ -0,0 +1,139 @@
+#-*- coding: utf-8 -*-
+"""
+@brief      test log(time=33s)
+"""
+
+import sys
+import os
+import unittest
+
+
+try:
+    import jyquickhelper as skip_
+except ImportError:
+    path = os.path.normpath(
+        os.path.abspath(
+            os.path.join(
+                os.path.split(__file__)[0],
+                "..",
+                "..",
+                "..",
+                "jyquickhelper",
+                "src")))
+    if path not in sys.path:
+        sys.path.append(path)
+    import jyquickhelper as skip_
+    
+
+try:
+    import pyquickhelper as skip__
+except ImportError:
+    path = os.path.normpath(
+        os.path.abspath(
+            os.path.join(
+                os.path.split(__file__)[0],
+                "..",
+                "..",
+                "..",
+                "pyquickhelper",
+                "src")))
+    if path not in sys.path:
+        sys.path.append(path)
+    import pyquickhelper as skip__
+    
+
+try:
+    import src
+except ImportError:
+    path = os.path.normpath(
+        os.path.abspath(
+            os.path.join(
+                os.path.split(__file__)[0],
+                "..",
+                "..")))
+    if path not in sys.path:
+        sys.path.append(path)
+    import src
+
+from pyquickhelper.loghelper import fLOG
+from pyquickhelper.pycode import get_temp_folder, is_travis_or_appveyor
+from pyquickhelper.ipythonhelper import execute_notebook_list
+from pyquickhelper.pycode import compare_module_version
+from pyquickhelper.ipythonhelper import install_python_kernel_for_unittest
+import IPython
+
+
+class TestRunNotebooksDsGarden(unittest.TestCase):
+
+    def test_run_notebook(self):
+        fLOG(
+            __file__,
+            self._testMethodName,
+            OutputPrint=__name__ == "__main__")
+
+        if sys.version_info[0] == 2:
+            # notebooks are not converted into python 2.7, so not tested
+            return
+
+        if compare_module_version(IPython.__version__, "4.0.0") < 0:
+            # IPython is not recnt enough
+            return
+
+        kernel_name = None if "travis" in sys.executable else install_python_kernel_for_unittest(
+            "python3_module_template")
+
+        temp = get_temp_folder(__file__, "temp_run_notebooks_dsgarden")
+
+        # selection of notebooks
+        fnb = os.path.normpath(os.path.join(
+            os.path.abspath(os.path.dirname(__file__)), "..", "..", "_doc", "notebooks", "dsgarden"))
+        keepnote = []
+        for f in os.listdir(fnb):
+            if os.path.splitext(f)[-1] == ".ipynb" and "long" not in f:
+                keepnote.append(os.path.join(fnb, f))
+        assert len(keepnote) > 0
+
+        # function to tell that a can be run
+        def valid(cell):
+            if "open_html_form" in cell:
+                return False
+            if "open_window_params" in cell:
+                return False
+            if '<div style="position:absolute' in cell:
+                return False
+            if "completion.dot" in cell and is_travis_or_appveyor() == "travis":
+                return False
+            return True
+
+        # additionnal path to add
+        addpaths = [os.path.normpath(os.path.join(
+            os.path.abspath(os.path.dirname(__file__)), "..", "..", "src")),
+            os.path.normpath(os.path.join(
+                os.path.abspath(os.path.dirname(__file__)), "..", "..", "..", "pyquickhelper", "src")),
+            os.path.normpath(os.path.join(
+                os.path.abspath(os.path.dirname(__file__)), "..", "..", "..", "jyquickhelper", "src"))
+        ]
+
+        # creation of a kernel
+        kernel_name = None if "travis" in sys.executable else install_python_kernel_for_unittest(
+            "mlstatpy")
+
+        # run the notebooks
+        res = execute_notebook_list(
+            temp, keepnote, fLOG=fLOG, valid=valid, additional_path=addpaths, kernel_name=kernel_name)
+
+        # final checkings
+        assert len(res) > 0
+        fails = [(os.path.split(k)[-1], v)
+                 for k, v in sorted(res.items()) if not v[0]]
+        for f in fails:
+            fLOG(f)
+        for k, v in sorted(res.items()):
+            name = os.path.split(k)[-1]
+            fLOG(name, v[0], v[1])
+        if len(fails) > 0:
+            raise fails[0][1][-1]
+
+
+if __name__ == "__main__":
+    unittest.main()
