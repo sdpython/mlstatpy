7bd59f430292a1c585a0554c7de76d7624b91548
xavier dupré <xavier.dupre@gmail.com>
2017-08-08 01:59:37 +0200

    licence

--- setup.py
+++ setup.py
@@ -203,6 +203,7 @@ if not r:
         version='%s%s' % (sversion, subversion),
         author='Xavier Dupré',
         author_email='xavier.dupre@gmail.com',
+        license="MIT",
         url="http://www.xavierdupre.fr/app/mlstatpy/helpsphinx/index.html",
         download_url="https://github.com/sdpython/mlstatpy/",
         description=DESCRIPTION,
