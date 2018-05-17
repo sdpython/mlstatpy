e4843def35d28e0f2f42bb5af9f09dc204198a58
xavier dupré <xavier.dupre@gmail.com>
2018-05-17 09:02:27 +0200

    minor fixes in setup, documentation, circleci

--- _doc/sphinxdoc/source/conf.py
+++ _doc/sphinxdoc/source/conf.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import sys
 import os
+from pyquickhelper.helpgen.default_conf import set_sphinx_variables, get_default_stylesheet
 
 choice = "bootstrap"
 
@@ -16,22 +17,10 @@ else:
     raise NotImplementedError()
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.split(__file__)[0])))
-sys.path.insert(
-    0,
-    os.path.abspath(
-        os.path.join(
-            os.path.split(__file__)[0],
-            "..",
-            "..",
-            "..",
-            "..",
-            "pyquickhelper",
-            "src")))
 
 local_template = os.path.join(os.path.abspath(
     os.path.dirname(__file__)), "phdoc_templates")
 
-from pyquickhelper.helpgen.default_conf import set_sphinx_variables, get_default_stylesheet
 set_sphinx_variables(__file__, "mlstatpy", "Xavier Dupré", 2018,
                      html_theme, html_theme_path, locals(),
                      extlinks=dict(
