34ab4d7023b7c0f9818b2f10da8a8d2d3bfdff5b
xavier dupré <xavier.dupre@gmail.com>
2019-01-13 16:08:57 +0100

    documentation

--- _doc/sphinxdoc/source/conf.py
+++ _doc/sphinxdoc/source/conf.py
@@ -21,7 +21,7 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.split(__file__)[0])))
 local_template = os.path.join(os.path.abspath(
     os.path.dirname(__file__)), "phdoc_templates")
 
-set_sphinx_variables(__file__, "mlstatpy", "Xavier Dupré", 2018,
+set_sphinx_variables(__file__, "mlstatpy", "Xavier Dupré", 2019,
                      html_theme, html_theme_path, locals(),
                      extlinks=dict(
                          issue=('https://github.com/sdpython/mlstatpy/issues/%s', 'issue')),
