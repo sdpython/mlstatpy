bb1cd540faf190b13360297555a6ee24c543afbb
xavier dupré <xavier.dupre@gmail.com>
2019-03-27 00:16:55 +0100

    Update test_graphviz.py

--- _unittests/ut_graph/test_graphviz.py
+++ _unittests/ut_graph/test_graphviz.py
@@ -4,12 +4,14 @@
 """
 import os
 import unittest
-from pyquickhelper.pycode import get_temp_folder
+from pyquickhelper.pycode import get_temp_folder, skipif_travis, skipif_appveyor
 from mlstatpy.graph.graphviz_helper import draw_graph_graphviz
 
 
 class TestGraphviz(unittest.TestCase):
 
+    @skipif_appveyor("no graphviz")
+    @skipif_travis("no graphviz")
     def test_make_video(self):
         temp = get_temp_folder(__file__, "temp_graphviz")
         fout = os.path.join(temp, "image.png")
