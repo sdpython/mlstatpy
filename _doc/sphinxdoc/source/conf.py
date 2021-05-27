c9ecf8d037c08536544536b7642035b540ac32d7
xavier dupré <xavier.dupre@gmail.com>
2021-05-27 02:28:37 +0200

    documentation

--- _doc/sphinxdoc/source/conf.py
+++ _doc/sphinxdoc/source/conf.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import sys
 import os
-from pyquickhelper.helpgen.default_conf import set_sphinx_variables, get_default_stylesheet
+from pyquickhelper.helpgen.default_conf import set_sphinx_variables
 
 choice = "pydata_sphinx_theme"
 
