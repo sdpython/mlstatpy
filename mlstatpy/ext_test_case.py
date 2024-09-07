db837f911dbbab71b5cd4deb55708cedf33583b0
xadupre <xadupre@microsoft.com>
2024-09-07 15:44:32 +0200

    fix

--- mlstatpy/ext_test_case.py
+++ mlstatpy/ext_test_case.py
@@ -1,9 +1,12 @@
 import os
+import stat
 import sys
+import time
 import unittest
+import unicodedata
 import warnings
 from contextlib import redirect_stderr, redirect_stdout
-from io import StringIO
+from io import StringIO, BytesIO
 from timeit import Timer
 from typing import Any, Callable, Dict, List, Optional, Union
 
@@ -123,7 +126,7 @@ def get_url_content_timeout(
         )
         return None
     except Exception as e:
-        if raise_exception:  # pragma: no cover
+        if raise_exception:
             raise InternetException(
                 f"Unable to retrieve content, url='{url}', exc={e}"
             ) from e
@@ -143,7 +146,7 @@ def get_url_content_timeout(
         if encoding is not None:
             try:
                 content = res.decode(encoding)
-            except UnicodeDecodeError as e:  # pragma: no cover
+            except UnicodeDecodeError as e:
                 # it tries different encoding
 
                 laste = [e]
@@ -243,9 +246,6 @@ def measure_time(
     for a better understanding of parameter *repeat* and *number*.
     The function returns a duration corresponding to
     *number* times the execution of the main statement.
-
-    .. versionchanged:: 0.4
-        Parameter *max_time* was added.
     """
     if not callable(stmt) and not isinstance(stmt, str):
         raise TypeError(
@@ -429,3 +429,344 @@ class ExtTestCase(unittest.TestCase):
         with redirect_stdout(sout), redirect_stderr(serr):
             res = fct()
         return res, sout.getvalue(), serr.getvalue()
+
+
+def remove_folder(top, remove_also_top=True, raise_exception=True):
+    """
+    Removes everything in folder *top*.
+
+    :param top: path to remove
+    :param remove_also_top: remove also root
+    :param raise_exception: raise an exception if a file cannot be remove
+    :return: list of removed files and folders
+        --> list of tuple ( (name, "file" or "dir") )
+    """
+    if top in {"", "C:", "c:", "C:\\", "c:\\", "d:", "D:", "D:\\", "d:\\"}:
+        raise RuntimeError(  # pragma: no cover
+            "top is a root (c: for example), this is not safe"
+        )
+
+    res = []
+    first_root = None
+    for root, dirs, files in os.walk(top, topdown=False):
+        for name in files:
+            t = os.path.join(root, name)
+            try:
+                os.remove(t)
+            except PermissionError as e:  # pragma: no cover
+                if raise_exception:
+                    raise PermissionError(f"unable to remove file {t}") from e
+                remove_also_top = False
+                continue
+            res.append((t, "file"))
+        for name in dirs:
+            t = os.path.join(root, name)
+            try:
+                os.rmdir(t)
+            except OSError as e:
+                if raise_exception:
+                    raise OSError(f"unable to remove folder {t}") from e
+                remove_also_top = False  # pragma: no cover
+                continue  # pragma: no cover
+            res.append((t, "dir"))
+        if first_root is None:
+            first_root = root
+
+    if top is not None and remove_also_top:
+        res.append((top, "dir"))
+        os.rmdir(top)
+
+    return res
+
+
+def get_temp_folder(
+    thisfile, name=None, clean=True, create=True, persistent=False, path_name="tpath"
+):
+    """
+    Creates and returns a local temporary folder to store files
+    when unit testing.
+
+    :param thisfile: use ``__file__`` or the function which runs the test
+    :param name: name of the temporary folder
+    :param clean: if True, clean the folder first, it can also a function
+        called to determine whether or not the folder should be cleaned
+    :param create: if True, creates it (empty if clean is True)
+    :param persistent: if True, create a folder at root level to reduce path length,
+        the function checks the ``MAX_PATH`` variable and
+        shorten the test folder is *max_path* is True on :epkg:`Windows`,
+        on :epkg:`Linux`, it creates a folder three level ahead
+    :param path_name: test path used when *max_path* is True
+    :return: temporary folder
+
+    The function extracts the file which runs this test and will name
+    the temporary folder base on the name of the method. *name* must be None.
+
+    Parameter *clean* can be a function.
+    Signature is ``def clean(folder)``.
+    """
+    if name is None:
+        name = thisfile.__name__
+        if name.startswith("test_"):
+            name = "temp_" + name[5:]
+        elif not name.startswith("temp_"):
+            name = "temp_" + name
+        thisfile = os.path.abspath(thisfile.__func__.__code__.co_filename)
+    final = os.path.split(name)[-1]
+
+    if not final.startswith("temp_") and not final.startswith("temp2_"):
+        raise NameError(f"the folder '{name}' must begin with temp_")
+
+    local = os.path.join(
+        os.path.normpath(os.path.abspath(os.path.dirname(thisfile))), name
+    )
+
+    if persistent:
+        if sys.platform.startswith("win"):
+            from ctypes.wintypes import MAX_PATH
+
+            if MAX_PATH <= 300:
+                local = os.path.join(os.path.abspath("\\" + path_name), name)
+            else:
+                local = os.path.join(local, "..", "..", "..", "..", path_name, name)
+        else:
+            local = os.path.join(local, "..", "..", "..", "..", path_name, name)
+        local = os.path.normpath(local)
+
+    if name == local:
+        raise NameError(f"The folder '{name}' must be relative, not absolute")
+
+    if not os.path.exists(local):
+        if create:
+            os.makedirs(local)
+            mode = os.stat(local).st_mode
+            nmode = mode | stat.S_IWRITE
+            if nmode != mode:
+                os.chmod(local, nmode)
+    else:
+        if (callable(clean) and clean(local)) or (not callable(clean) and clean):
+            # delayed import to speed up import time of pycode
+            remove_folder(local)
+            time.sleep(0.1)
+        if create and not os.path.exists(local):
+            os.makedirs(local)
+            mode = os.stat(local).st_mode
+            nmode = mode | stat.S_IWRITE
+            if nmode != mode:
+                os.chmod(local, nmode)
+
+    return local
+
+
+def noLOG(*args, **kwargs):
+    pass
+
+
+def unzip_files(
+    zipf, where_to=None, fLOG=noLOG, fvalid=None, remove_space=True, fail_if_error=True
+):
+    """
+    Unzips files from a zip archive.
+
+    :param zipf: archive (or bytes or BytesIO)
+    :param where_to: destination folder (can be None, the result is a list of tuple)
+    :param fLOG: logging function
+    :param fvalid: function which takes two paths (zip name, local name)
+        and return True if the file
+        must be unzipped, False otherwise, if None, the default answer is True
+    :param remove_space: remove spaces in created local path (+ ``',()``)
+    :param fail_if_error: fails if an error is encountered
+        (typically a weird character in a filename),
+        otherwise a warning is thrown.
+    :return: list of unzipped files
+    """
+    import zipfile
+
+    if isinstance(zipf, bytes):
+        zipf = BytesIO(zipf)
+
+    try:
+        with zipfile.ZipFile(zipf, "r"):
+            pass
+    except zipfile.BadZipFile as e:  # pragma: no cover
+        if isinstance(zipf, BytesIO):
+            raise e
+        raise OSError(f"Unable to read file '{zipf}'") from e
+
+    files = []
+    with zipfile.ZipFile(zipf, "r") as file:
+        for info in file.infolist():
+            if fLOG:
+                fLOG(f"[unzip_files] unzip '{info.filename}'")
+            if where_to is None:
+                try:
+                    content = file.read(info.filename)
+                except zipfile.BadZipFile as e:  # pragma: no cover
+                    if fail_if_error:
+                        raise zipfile.BadZipFile(
+                            f"Unable to extract '{info.filename}' due to {e}"
+                        ) from e
+                    warnings.warn(
+                        f"Unable to extract '{info.filename}' due to {e}",
+                        UserWarning,
+                        stacklevel=0,
+                    )
+                    continue
+                files.append((info.filename, content))
+            else:
+                clean = remove_diacritics(info.filename)
+                if remove_space:
+                    clean = (
+                        clean.replace(" ", "")
+                        .replace("'", "")
+                        .replace(",", "_")
+                        .replace("(", "_")
+                        .replace(")", "_")
+                    )
+                tos = os.path.join(where_to, clean)
+                if not os.path.exists(tos):
+                    if fvalid and not fvalid(info.filename, tos):
+                        fLOG("[unzip_files]    skipping", info.filename)
+                        continue
+                    try:
+                        data = file.read(info.filename)
+                    except zipfile.BadZipFile as e:  # pragma: no cover
+                        if fail_if_error:
+                            raise zipfile.BadZipFile(
+                                f"Unable to extract '{info.filename}' due to {e}"
+                            ) from e
+                        warnings.warn(
+                            f"Unable to extract '{info.filename}' due to {e}",
+                            UserWarning,
+                            stacklevel=0,
+                        )
+                        continue
+                    # check encoding to avoid characters not allowed in paths
+                    if not os.path.exists(tos):
+                        if sys.platform.startswith("win"):
+                            tos = tos.replace("/", "\\")
+                        finalfolder = os.path.split(tos)[0]
+                        if not os.path.exists(finalfolder):
+                            fLOG(
+                                "[unzip_files]    creating folder (zip)",
+                                os.path.abspath(finalfolder),
+                            )
+                            try:
+                                os.makedirs(finalfolder)
+                            except FileNotFoundError as e:  # pragma: no cover
+                                mes = (
+                                    "Unexpected error\ninfo.filename={0}\ntos={1}"
+                                    "\nfinalfolder={2}\nlen(nfinalfolder)={3}"
+                                ).format(
+                                    info.filename, tos, finalfolder, len(finalfolder)
+                                )
+                                raise FileNotFoundError(mes) from e
+                        if not info.filename.endswith("/"):
+                            try:
+                                with open(tos, "wb") as u:
+                                    u.write(data)
+                            except FileNotFoundError as e:  # pragma: no cover
+                                # probably an issue in the path name
+                                # the next lines are just here to distinguish
+                                # between the two cases
+                                if not os.path.exists(finalfolder):
+                                    raise e
+                                newname = info.filename.replace(" ", "_").replace(
+                                    ",", "_"
+                                )
+                                if sys.platform.startswith("win"):
+                                    newname = newname.replace("/", "\\")
+                                tos = os.path.join(where_to, newname)
+                                finalfolder = os.path.split(tos)[0]
+                                if not os.path.exists(finalfolder):
+                                    fLOG(
+                                        "[unzip_files]    creating folder (zip)",
+                                        os.path.abspath(finalfolder),
+                                    )
+                                    os.makedirs(finalfolder)
+                                with open(tos, "wb") as u:
+                                    u.write(data)
+                            files.append(tos)
+                            fLOG(
+                                "[unzip_files]    unzipped ", info.filename, " to ", tos
+                            )
+                    elif not tos.endswith("/"):  # pragma: no cover
+                        files.append(tos)
+                elif not info.filename.endswith("/"):  # pragma: no cover
+                    files.append(tos)
+    return files
+
+
+def ungzip_files(
+    filename,
+    where_to=None,
+    fLOG=noLOG,
+    fvalid=None,
+    remove_space=True,
+    unzip=True,
+    encoding=None,
+):
+    """
+    Uncompresses files from a gzip file.
+
+    :param filename: final gzip file (double compression, extension
+        should something like .zip.gz)
+    :param where_to: destination folder (can be None, the result is a list of tuple)
+    :param fLOG: logging function
+    :param fvalid: function which takes two paths (zip name, local name)
+        and return True if the file
+        must be unzipped, False otherwise, if None, the default answer is True
+    :param remove_space: remove spaces in created local path (+ ``',()``)
+    :param unzip: unzip file after gzip
+    :param encoding: encoding
+    :return: list of unzipped files
+    """
+    import gzip
+
+    if isinstance(filename, bytes):
+        is_file = False
+        filename = BytesIO(filename)
+    else:
+        is_file = True
+
+    if encoding is None:
+        f = gzip.open(filename, "rb")
+        content = f.read()
+        f.close()
+        if unzip:
+            try:
+                return unzip_files(content, where_to=where_to, fLOG=fLOG)
+            except Exception as e:
+                raise OSError(f"Unable to unzip file '{filename}'") from e
+        elif where_to is not None:
+            filename = os.path.split(filename)[-1].replace(".gz", "")
+            filename = os.path.join(where_to, filename)
+            with open(filename, "wb") as f:
+                f.write(content)
+            return filename
+        return content
+    else:
+        f = gzip.open(filename, "rt", encoding="utf-8")
+        content = f.read()
+        f.close()
+        if is_file:
+            filename = filename.replace(".gz", "")
+            with open(filename, "wb") as f:
+                f.write(content)
+            return filename
+        return content
+
+
+def remove_diacritics(input_str):
+    """
+    Removes diacritics.
+
+    :param input_str: string to clean
+    :return: cleaned string
+
+    Example::
+
+        enguérand --> enguerand
+    """
+    nkfd_form = unicodedata.normalize("NFKD", input_str)
+    only_ascii = nkfd_form.encode("ASCII", "ignore")
+    return only_ascii.decode("utf8")
